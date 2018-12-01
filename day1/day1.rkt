699ab4ab0896f26880291044812306a569926e90
Russell Blickhan <rwblickhan@gmail.com>
2018-12-01 00:17:29 -0800

    Finish day 1 with slightly ugly Racket script

---
+++ day1/day1.rkt
@@ -0,0 +1,22 @@
+#lang racket
+
+(require racket/set)
+(require data/queue)
+
+(define in (map string->number (string-split (port->string (open-input-file "input")))))
+
+(pretty-print (foldl + 0 in))
+
+(define (loop-freqs seen-set acc)
+  (loop-freqs-helper in seen-set acc))
+
+
+(define (loop-freqs-helper freqs seen-set acc)
+  (let ([new-acc (+ acc (first freqs))])
+    (if (set-member? seen-set new-acc)
+      new-acc
+      (if (empty? (rest freqs))
+        (loop-freqs (set-add seen-set new-acc) new-acc)
+        (loop-freqs-helper (rest freqs) (set-add seen-set new-acc) new-acc)))))
+
+(pretty-print (loop-freqs (set 0) 0))
